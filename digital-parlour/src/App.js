dc7a2e64d00bc97d3f3c8225fca46fca35321ae1
Shuvo <74643721+AhmdShuvo@users.noreply.github.com>
2021-11-08 14:08:58 +0600

    init

---
+++ digital-parlour/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+
+function App() {
+  return (
+     
+    <BrowserRouter>
+    <Switch>
+      <Route>
+
+      </Route>
+    </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
